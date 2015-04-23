ef31a334ca0db40c5a7cff3bed798b78f6600ccd
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-23 09:34:39 +0800

    1.  Add in cpp file and its header.

---
+++ WrapCPP/Cpp.cpp
@@ -0,0 +1,14 @@
+//
+//  Cpp.cpp
+//  WrapCPP
+//
+//  Created by Calvin Cheng on 22/4/15.
+//  Copyright (c) 2015 Algo Access Pte. Ltd. All rights reserved.
+//
+
+#include "Cpp.h"
+
+double timesTwo(double number)
+{
+    return 2 * number;
+}
---
+++ WrapCPP/Cpp.h
@@ -0,0 +1,16 @@
+//
+//  Cpp.h
+//  WrapCPP
+//
+//  Created by Calvin Cheng on 22/4/15.
+//  Copyright (c) 2015 Algo Access Pte. Ltd. All rights reserved.
+//
+
+#ifndef __WrapCPP__Cpp__
+#define __WrapCPP__Cpp__
+
+#include <stdio.h>
+
+double timesTwo(double number);
+
+#endif /* defined(__WrapCPP__Cpp__) */
